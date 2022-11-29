eb111ba56d8bb2cd97c3e71652bc1338cb15b245
Pasquale Aragosa <aragosap@gmail.com>
2022-11-29 16:46:11 +0100

    inizial commit

--- index.php
+++ index.php
@@ -20,8 +20,6 @@ if (isset($_GET['size'])) {
     }
 
     $password = getRandomPassword($size);
-
-    echo $password;
 }
 
 ?>
@@ -34,15 +32,46 @@ if (isset($_GET['size'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        .wrapper {
+            width: 80%;
+            margin: auto;
+            /* background-color: blue; */
+        }
+
+
+        .content {
+            width: 500px;
+            margin: auto;
+            /* background-color: red; */
+        }
+    </style>
 </head>
 
 <body>
-    <form action="index.php" method="get">
-        <label for="size">Lunghezza Password: </label>
-        <input name="size" id="size" type="number">
-        <button type="submit">Invia</button>
-        <button type="reset">Resetta</button>
-    </form>
+
+    <div class="wrapper text-center mt-5">
+        <header>
+            <h1>Strong Password Generator</h1>
+            <h2>Genera una password sicura</h2>
+        </header>
+        <main>
+            <div class="content mt-4">
+                <form action="index.php" method="get">
+                    <label for="size">Lunghezza Password: </label>
+                    <input name="size" id="size" type="number">
+                    <button type="submit">Invia</button>
+                    <button type="reset">Resetta</button>
+                </form>
+
+                <p class="text-start">Password: <?php echo $password; ?> </p>
+            </div>
+            <!-- /.content -->
+        </main>
+    </div>
+    <!-- /.wrapper -->
+
 </body>
 
 </html>
